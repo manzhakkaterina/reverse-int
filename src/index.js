a6cc88a3546460d719c2cd7425e2dac74c82cec1
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-01-15 19:54:20 +0200

    fix: fix mistakes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 module.exports = function reverse (n) {
 let answer = '';
 let str_answer = '';
-str_answer = String(number);
+if (n < 0) {n = Math.abs(n);}
+str_answer = String(n);
 let kol = str_answer.length;
 let j = kol - 1;
   for (let i = 0; i <= kol - 1; i++) {
