b8f2003eb913d92208127dbd9299c5506b6da871
Katerina Manzhak <59136296+manzhakkaterina@users.noreply.github.com>
2023-01-15 19:40:05 +0200

    fix: add programm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+let answer = '';
+let str_answer = '';
+str_answer = String(number);
+let kol = str_answer.length;
+let j = kol - 1;
+  for (let i = 0; i <= kol - 1; i++) {
+    answer = answer + str_answer[j];
+    j--;
+  }
+  return Number(answer); 
 }
